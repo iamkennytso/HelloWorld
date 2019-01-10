65a8f9f2ec4272d102fd44639d0b0a0bad7a7933
Kenneth Tso <iamkennytso@gmail.com>
2019-01-10 15:44:49 -0500

    show hide by placements

--- src/components/ClothingPlacement/ClothingPlacement.jsx
+++ src/components/ClothingPlacement/ClothingPlacement.jsx
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './ClothingPlacement.scss';
 import ClothingArticle from '../ClothingArticle/ClothingArticle';
+import { timingSafeEqual } from 'crypto';
 
-const ClothingPlacement = ({placement, clothes, clothingClick}) => {
-  return (
-    <div className={`ClothingPlacement ClothingPlacement-${placement}`}>
-      {placement} <br />
-      <div className='ClothingPlacement-Container'>
-        {clothes.map(article => {
-          return <ClothingArticle key={article.title} article={article} clothingClick={clothingClick} placement={placement} />
-        })}
+const INITIAL_STATE = {
+  show: true
+};
+
+class ClothingPlacement extends Component {
+  constructor(props) {
+    super(props);
+    this.state = INITIAL_STATE;
+  };
+
+  toggleShow = () => {
+    this.setState({ show: !this.state.show })
+  }
+
+  render () {
+    const { placement, clothes, clothingClick } = this.props
+    return (
+      <div className={`ClothingPlacement ClothingPlacement-${placement}`}>
+        <div className='ClothingPlacement-Title' onClick={this.toggleShow}>
+          {placement}
+        </div>
+        {this.state.show 
+        ? <div className='ClothingPlacement-Container'>
+            {clothes.map(article => {
+              return <ClothingArticle key={article.title} article={article} clothingClick={clothingClick} placement={placement} />
+            })}
+          </div>
+        : null}
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default ClothingPlacement;
+

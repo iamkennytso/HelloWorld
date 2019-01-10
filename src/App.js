976f47a2239dd018c1ba9c5ea58d740edbb853a7
Kenneth Tso <iamkennytso@gmail.com>
2019-01-10 09:30:17 -0500

    hello world

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import './App.scss';
+
+const INITIAL_STATE = {
+  hello: ''
+};
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = INITIAL_STATE;
+  };
+
+  componentDidMount () {
+    this.getHello()
+  };
+
+  getHello = async () => {
+    try {
+      const { data: hello } = await axios.get('https://boiler010919-81804.firebaseio.com/hello.json');
+      this.setState({ hello });
+    } catch (err) {
+      console.error(err)
+      this.setState({ hello: 'localHost'});
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        Hello {this.state.hello}
       </div>
     );
   }
